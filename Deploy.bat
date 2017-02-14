c5ce539768614b95a7e373e4972e5007e214a0e8
duduluu@outlook.com <duduluu@outlook.com>
2017-02-14 18:00:07 +0800

    Add Steam Workshop Published File ID

--- Deploy.bat
+++ Deploy.bat
@@ -4,9 +4,10 @@ rd /s /q D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Defs
 rd /s /q D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Languages
 rd /s /q D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Sounds
 rd /s /q D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Textures
-xcopy About D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\About /e
-xcopy Assemblies D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Assemblies /e
-xcopy Defs D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Defs /e
-xcopy Languages D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Languages /e
-xcopy Sounds D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Sounds /e
-xcopy Textures D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Textures /e
+xcopy About D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\About/e /d /i /y
+xcopy Assemblies D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Assemblies/e /d /i /y
+xcopy Defs D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Defs/e /d /i /y
+xcopy Languages D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Languages/e /d /i /y
+xcopy Sounds D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Sounds/e /d /i /y
+xcopy Textures D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\Textures/e /d /i /y
+copy LICENSE D:\Game\Steam\steamapps\common\RimWorld\Mods\Industrialisation\LICENSE /y
